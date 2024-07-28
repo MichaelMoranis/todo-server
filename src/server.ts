2a81885c45ce2ee7f25efec8f896d428654a5aa5
michaelmoranis <michaelmoranesul@gmail.com>
2024-07-28 10:15:55 -0300

    access: somente para app-tarefa.vercel.app

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const database = new DatabasePostgres();
 const server = fastify();
 
 server.register(cors, {
-  origin: ["*"], // Permitir a origem do seu frontend local e em produção
+  origin: ["app-tarefa.vercel.app"], // Permitir a origem do seu frontend local e em produção
   methods: ["GET", "POST", "PUT", "DELETE"], // Permitir os métodos que você vai usar
 });
 
